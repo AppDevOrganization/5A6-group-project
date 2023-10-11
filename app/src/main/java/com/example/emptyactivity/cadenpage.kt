367a5f2fbfe383cd3bc46369e2aec5757eaa8607
c-marion <2168153@johnabbottcollege.net>
2023-10-11 17:03:25 -0400

    adding things to my page

--- app/src/main/java/com/example/emptyactivity/cadenpage.kt
+++ app/src/main/java/com/example/emptyactivity/cadenpage.kt
@@ -1,2 +1,22 @@
 package com.example.emptyactivity
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CadenPage(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primaryContainer)
+    ) {
+        Text(
+            text = "This is the caden page"
+        )
+    }
+}
