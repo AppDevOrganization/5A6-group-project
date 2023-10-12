44ac8ff5972d8485784e6141a465ae7d63959585
jco898 <62203980+jco898@users.noreply.github.com>
2023-10-12 19:38:10 -0400

    Update WithdrawalScreen.kt
    - Added Surface to Preview function

--- app/src/main/java/com/example/emptyactivity/WithdrawalScreen.kt
+++ app/src/main/java/com/example/emptyactivity/WithdrawalScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,6 +16,7 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -145,7 +147,12 @@ class WithdrawalScreen {
     @Composable
     fun WithdrawalScreenPreview() {
         EmptyActivityTheme {
-            ShowWithdrawalScreen()
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                WithdrawalScreen().ShowWithdrawalScreen()
+            }
         }
     }
 
