ab01d157d0316b3e071575f507f5924e9d40dbcf
jeffandersoncharriot <98402030+jeffandersoncharriot@users.noreply.github.com>
2023-11-02 22:32:11 -0400

    - Created logo screen in startup with LaunchedEffect and rememberUpdatedState - Start ups with logo for 2 seconds

--- app/src/main/java/com/example/emptyactivity/MainActivity.kt
+++ app/src/main/java/com/example/emptyactivity/MainActivity.kt
@@ -12,6 +12,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -40,10 +43,25 @@ class MainActivity : ComponentActivity() {
                    // Greeting("Android")
                     // WithdrawalScreen().ShowWithdrawalScreen()
                     // CadenPage().MainPage(breathList = SAMPLE_LIST)
-                    CJJBankApp()
+
+MainScreen()
+
+
                 }
             }
         }
+    }
+}
+
+
+@Composable
+fun MainScreen()
+{
+    var showStartupScreen by remember { mutableStateOf(true) }
+    if (showStartupScreen) {
+        LandingScreen(onTimeout = { showStartupScreen = false })
+    } else {
+        CJJBankApp()
     }
 }
 
