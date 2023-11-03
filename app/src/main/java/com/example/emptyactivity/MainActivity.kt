3ae035804d9baeb4f9d3d94615c044e8a71d40e4
jeffandersoncharriot <98402030+jeffandersoncharriot@users.noreply.github.com>
2023-11-03 00:58:10 -0400

    Navigation drawer implemented - has Modal Navigation Drawer and a set of navigation drawer items and a floating action button to control the drawer's state. - created the state of the navigation drawer, manages whether it is open or closed. - a coroutinescope used to launch coroutines for asynchronous operations.

--- app/src/main/java/com/example/emptyactivity/MainActivity.kt
+++ app/src/main/java/com/example/emptyactivity/MainActivity.kt
@@ -4,16 +4,35 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -22,10 +41,13 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.emptyactivity.components.BankTabRow
 import com.example.emptyactivity.ui.theme.EmptyActivityTheme
+import kotlinx.coroutines.launch
+import org.w3c.dom.Text
 
 val SAMPLE_LIST = mutableListOf<Double>(5.01, 2.4, 6.4, 8.7, 5.9)
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -36,16 +58,90 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-                   //AccountScreen();
-                   // Greeting("Android")
+                    //AccountScreen();
+                    // Greeting("Android")
                     // WithdrawalScreen().ShowWithdrawalScreen()
                     // CadenPage().MainPage(breathList = SAMPLE_LIST)
-                    CJJBankApp()
+                    MainScreen()
+
                 }
             }
         }
     }
 }
+
+
+/**
+ * This is the main screen of the application that includes a Modal Navigation Drawer
+ * with a set of navigation drawer items and a floating action button to control the drawer's state.
+ *
+ * @see https://developer.android.com/jetpack/compose/components/drawer for managing the state of the navigation drawer.
+ * @param drawerState The state of the navigation drawer, manages whether it is open or closed.
+ * @param scope A [CoroutineScope] used to launch coroutines for asynchronous operations.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                // Title of the drawer
+                Text("NAME HERE", modifier = Modifier.padding(16.dp))
+                Divider()
+
+                // Navigation drawer items
+                NavigationDrawerItem(
+                    icon = { Icon(Icons.Filled.Home, contentDescription = "") },
+                    label = { Text("Home") },
+                    selected = false,
+                    onClick = { /* Handle click for "Home" */ }
+                )
+                NavigationDrawerItem(
+                    icon = { Icon(Icons.Filled.Send, contentDescription = "") },
+                    label = { Text("Transfers") },
+                    selected = false,
+                    onClick = { /* Handle click for "Transfers" */ }
+                )
+                NavigationDrawerItem(
+                    icon = { Icon(Icons.Filled.Settings, contentDescription = "") },
+                    label = { Text("Settings") },
+                    selected = false,
+                    onClick = { /* Handle click for "Settings" */ }
+                )
+                NavigationDrawerItem(
+                    icon = { Icon(Icons.Filled.ExitToApp, contentDescription = "") },
+                    label = { Text("Logout") },
+                    selected = false,
+                    onClick = { /* Handle click for "Logout" */ }
+                )
+            }
+        },
+    ) {
+        Scaffold(
+            floatingActionButton = {
+                ExtendedFloatingActionButton(
+                    text = { Text("More") },
+                    icon = { Icon(Icons.Filled.AccountCircle, contentDescription = "") },
+                    onClick = {
+                        scope.launch {
+                            drawerState.apply {
+                                if (isClosed) open() else close()
+                            }
+                        }
+                    }
+                )
+            }
+        ) {
+            it
+           CJJBankApp()
+        }
+    }
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -112,15 +208,13 @@ fun NavHostController.navigateSingleTopTo(route: String) =
 
 @Preview
 @Composable
-fun CJJBankAppPreview()
-{
+fun CJJBankAppPreview() {
     CJJBankApp()
 }
 
 @Preview
 @Composable
-fun CJJBankAppDarkModePreview()
-{
+fun CJJBankAppDarkModePreview() {
     EmptyActivityTheme(darkTheme = true) {
         CJJBankApp()
     }
