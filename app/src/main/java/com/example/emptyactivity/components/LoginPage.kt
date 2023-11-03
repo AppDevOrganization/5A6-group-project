18141d37f4dec1e08a6a70c03f4e522eba5ddf23
c-marion <2168153@johnabbottcollege.net>
2023-11-03 14:31:47 -0400

    Began the real login page + preview

--- app/src/main/java/com/example/emptyactivity/components/LoginPage.kt
+++ app/src/main/java/com/example/emptyactivity/components/LoginPage.kt
@@ -1,2 +1,22 @@
 package com.example.emptyactivity.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun LoginPage(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+    ) {
+        
+    }
+}
+
+@Preview
+@Composable
+fun LoginPagePreview(modifier: Modifier = Modifier) {
+    LoginPage(modifier)
+}
