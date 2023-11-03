821f623e2175aa387583c6dc57657ea7d4d42af2
c-marion <2168153@johnabbottcollege.net>
2023-11-03 15:24:17 -0400

    added more to the login page

--- app/src/main/java/com/example/emptyactivity/components/LoginPage.kt
+++ app/src/main/java/com/example/emptyactivity/components/LoginPage.kt
@@ -1,17 +1,58 @@
 package com.example.emptyactivity.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginPage(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ) {
-        
+        Text(
+            text = "Log In",
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold,
+        )
+        TextField(
+            value = "Email",
+            onValueChange = {}
+        )
+        TextField(
+            value = "Password",
+            onValueChange = {}
+        )
+        Button(
+            modifier = Modifier
+                .padding(10.dp)
+                .width(200.dp)
+                .height(50.dp),
+            onClick = {}
+        ) {
+            Text(
+                text = "Log in",
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
     }
 }
 
