3cf68b03f143444c4eee0d523ef4c9f1479a4e52
c-marion <2168153@johnabbottcollege.net>
2023-11-29 16:47:42 -0500

    Created AuthRepositoryFirebase that implements AuthRepository

---
+++ app/src/main/java/com/example/emptyactivity/data/AuthRepositoryFirebase.java
@@ -0,0 +1,28 @@
+package com.example.emptyactivity.data;
+
+public class AuthRepositoryFirebase implements AuthRepository {
+    @Override
+    public void currentUser() {
+
+    }
+
+    @Override
+    public void signUp() {
+
+    }
+
+    @Override
+    public void signIn() {
+
+    }
+
+    @Override
+    public void signOut() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
