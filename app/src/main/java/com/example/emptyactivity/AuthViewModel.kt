708297ab7dd9bc973d6c97ad4be248e57dd9cc0e
c-marion <2168153@johnabbottcollege.net>
2023-11-29 16:48:58 -0500

    Created AuthViewModel

---
+++ app/src/main/java/com/example/emptyactivity/AuthViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.emptyactivity
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.emptyactivity.data.AuthRepository
+
+@Composable
+fun AuthViewModel(
+    authRepository: AuthRepository,
+    modifier: Modifier = Modifier
+) {
+
+}
+
