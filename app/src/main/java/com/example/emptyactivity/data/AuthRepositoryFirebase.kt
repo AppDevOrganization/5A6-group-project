a21106a12f68e2926e710845823ba3f99242c318
c-marion <2168153@johnabbottcollege.net>
2023-11-29 20:38:39 -0500

    implemented the methods

--- app/src/main/java/com/example/emptyactivity/data/AuthRepositoryFirebase.kt
+++ app/src/main/java/com/example/emptyactivity/data/AuthRepositoryFirebase.kt
@@ -1,4 +1,24 @@
 package com.example.emptyactivity.data
 
-class AuthRepositoryFirebase {
+class AuthRepositoryFirebase: AuthRepository {
+    override fun currentUser() {
+        TODO("Not yet implemented")
+    }
+
+    override fun signUp() {
+        TODO("Not yet implemented")
+    }
+
+    override fun signIn() {
+        TODO("Not yet implemented")
+    }
+
+    override fun signOut() {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete() {
+        TODO("Not yet implemented")
+    }
+
 }
