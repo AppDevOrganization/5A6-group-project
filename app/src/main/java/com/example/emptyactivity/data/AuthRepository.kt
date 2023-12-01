99341c26b925c207c2ce82ad029ab7d5d4b30f57
c-marion <2168153@johnabbottcollege.net>
2023-11-30 19:08:33 -0500

    set currentUser return type to StateFlow<User?>

--- app/src/main/java/com/example/emptyactivity/data/AuthRepository.kt
+++ app/src/main/java/com/example/emptyactivity/data/AuthRepository.kt
@@ -3,7 +3,7 @@ package com.example.emptyactivity.data
 import kotlinx.coroutines.flow.StateFlow
 
 interface AuthRepository {
-    fun currentUser(): StateFlow<User>
+    fun currentUser(): StateFlow<User?>
     suspend fun signUp(email: String, password: String): Boolean
     suspend fun signIn(email: String, password: String): Boolean
     fun signOut()
