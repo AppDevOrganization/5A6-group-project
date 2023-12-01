7e89c7634ad4d878b00a4e9c6a3d6e624cd8047b
c-marion <2168153@johnabbottcollege.net>
2023-12-01 12:15:13 -0500

    trying to fix errors

--- app/src/main/java/com/example/emptyactivity/data/AppModule.kt
+++ app/src/main/java/com/example/emptyactivity/data/AppModule.kt
@@ -1,16 +1,15 @@
 package com.example.emptyactivity.data
 
 import android.content.Context
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
+import com.google.firebase.ktx.Firebase
 
 class AppModule(
     private val appContext: Context
 ) {
     /* Create appropriate repository (backed by a DataStore) on first use.
     Only one copy will be created during lifetime of the application. */
-    val profileRepository : ProfileRepository by lazy {
-        ProfileRepositoryDataStore(appContext)
+    val profileRepository : UserPreferencesRepository by lazy {
+        UserPreferencesRepository(context = appContext)
     }
     val authRepository : AuthRepository by lazy {
         AuthRepositoryFirebase(Firebase.auth)
