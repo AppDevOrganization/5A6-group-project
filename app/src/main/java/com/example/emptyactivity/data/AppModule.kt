ba8ae6e6a6f1f97da73e3c3fec3394eb8652acd5
c-marion <2168153@johnabbottcollege.net>
2023-11-30 20:43:40 -0500

    added authRepository value

--- app/src/main/java/com/example/emptyactivity/data/AppModule.kt
+++ app/src/main/java/com/example/emptyactivity/data/AppModule.kt
@@ -1,9 +1,15 @@
 package com.example.emptyactivity.data
 
 import android.content.Context
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class AppModule(
     private val appContext: Context
 ) {
-    
+    /* Create appropriate repository (backed by a DataStore) on first use.
+    Only one copy will be created during lifetime of the application. */
+    val authRepository : AuthRepository by lazy {
+        AuthRepositoryFirebase(Firebase.auth)
+    }
 }
