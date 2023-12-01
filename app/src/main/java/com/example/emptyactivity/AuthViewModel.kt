db4e9893e22ceab16998fa54a12741d78c0ad3cd
c-marion <2168153@johnabbottcollege.net>
2023-11-30 20:18:57 -0500

    added currentUser()

--- app/src/main/java/com/example/emptyactivity/AuthViewModel.kt
+++ app/src/main/java/com/example/emptyactivity/AuthViewModel.kt
@@ -3,12 +3,16 @@ package com.example.emptyactivity
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.emptyactivity.data.AuthRepository
+import com.example.emptyactivity.data.User
+import kotlinx.coroutines.flow.StateFlow
 
 @Composable
 fun AuthViewModel(
     authRepository: AuthRepository,
     modifier: Modifier = Modifier
 ) {
-
+    fun currentUser(): StateFlow<User?> {
+        return authRepository.currentUser()
+    }
 }
 
