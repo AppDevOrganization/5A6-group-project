66a1dba23d5f4d24fe74d18e135946c425b0edcf
c-marion <2168153@johnabbottcollege.net>
2023-11-30 19:02:07 -0500

    added suspend modifier to most functions

--- app/src/main/java/com/example/emptyactivity/data/AuthRepository.kt
+++ app/src/main/java/com/example/emptyactivity/data/AuthRepository.kt
@@ -3,9 +3,9 @@ package com.example.emptyactivity.data
 import kotlinx.coroutines.flow.StateFlow
 
 interface AuthRepository {
-    fun currentUser(): StateFlow<User>
-    fun signUp(email: String, password: String): Boolean
+    suspend fun currentUser(): StateFlow<User>
+    suspend fun signUp(email: String, password: String): Boolean
     fun signIn(email: String, password: String): Boolean
     fun signOut()
-    fun delete()
+    suspend fun delete()
 }
