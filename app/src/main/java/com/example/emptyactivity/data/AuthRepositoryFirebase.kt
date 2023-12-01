6023b8c5c0a1c8aa74994831fec6b2a0d3ffedde
c-marion <2168153@johnabbottcollege.net>
2023-11-30 19:02:51 -0500

    added suspend to most functions and implemented signUp()

--- app/src/main/java/com/example/emptyactivity/data/AuthRepositoryFirebase.kt
+++ app/src/main/java/com/example/emptyactivity/data/AuthRepositoryFirebase.kt
@@ -4,16 +4,22 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.tasks.await
 
 class AuthRepositoryFirebase(private val auth: FirebaseAuth): AuthRepository {
     private val currentUserStateFlow = MutableStateFlow(auth.currentUser?.toUser())
 
-    override fun currentUser(): StateFlow<User> {
+    override suspend fun currentUser(): StateFlow<User> {
         TODO("Not yet implemented")
     }
 
-    override fun signUp(email: String, password: String): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun signUp(email: String, password: String): Boolean {
+        return try {
+            auth.createUserWithEmailAndPassword(email, password).await()
+            return true
+        } catch(e: Exception) {
+            return false
+        }
     }
 
     override fun signIn(email: String, password: String): Boolean {
@@ -24,7 +30,7 @@ class AuthRepositoryFirebase(private val auth: FirebaseAuth): AuthRepository {
         TODO("Not yet implemented")
     }
 
-    override fun delete() {
+    override suspend fun delete() {
         TODO("Not yet implemented")
     }
 
