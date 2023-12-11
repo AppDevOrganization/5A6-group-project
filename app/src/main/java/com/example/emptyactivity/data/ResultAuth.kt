a8d6a6f3529bd4746db8a9958cd0b5d8d3f37d82
c-marion <2168153@johnabbottcollege.net>
2023-12-11 14:14:13 -0500

    implemented ResultAuth

--- app/src/main/java/com/example/emptyactivity/data/ResultAuth.kt
+++ app/src/main/java/com/example/emptyactivity/data/ResultAuth.kt
@@ -1,3 +1,8 @@
 package com.example.emptyactivity.data
 
-sealed class ResultAuth
+sealed class ResultAuth<out T> {
+    data class Success<out T>(val data: T): ResultAuth<T>()
+    data class Failure(val exception: Throwable): ResultAuth<Nothing>()
+    object Inactive: ResultAuth<Nothing>()
+    object InProgress: ResultAuth<Nothing>()
+}
