aa4846d87352d4cdb0e3f1179c23c292b56d50b7
jeffandersoncharriot <jeffandersoncharriot@gmail.com>
2023-12-11 12:19:39 -0500

    uncommented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ android {
 dependencies {
     implementation("androidx.compose.runtime:runtime:1.0.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-    //implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
