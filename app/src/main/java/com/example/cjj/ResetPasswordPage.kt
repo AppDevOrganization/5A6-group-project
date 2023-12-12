9c4c532579c7ac12aa560c241a0d3c07e6bfa834
c-marion <2168153@johnabbottcollege.net>
2023-12-12 13:40:10 -0500

    Created ResetPasswordPage

---
+++ app/src/main/java/com/example/cjj/ResetPasswordPage.kt
@@ -0,0 +1,12 @@
+package com.example.cjj
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ResetPasswordPage(
+
+    modifier: Modifier = Modifier
+) {
+
+}
