524c548281826b4e22aa54dfcc4ba66ca92803d5
c-marion <2168153@johnabbottcollege.net>
2023-12-12 13:43:19 -0500

    added authViewModel and goToLogin params

--- app/src/main/java/com/example/cjj/ResetPasswordPage.kt
+++ app/src/main/java/com/example/cjj/ResetPasswordPage.kt
@@ -2,10 +2,12 @@ package com.example.cjj
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.emptyactivity.data.AuthViewModel
 
 @Composable
 fun ResetPasswordPage(
-
+    authViewModel: AuthViewModel,
+    goToLogin: () -> Unit = {},
     modifier: Modifier = Modifier
 ) {
 
