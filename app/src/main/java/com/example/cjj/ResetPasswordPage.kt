4419d62600a2a1db825e6fb9339da8c3ce0fe653
c-marion <2168153@johnabbottcollege.net>
2023-12-12 14:08:58 -0500

    Created the UI

--- app/src/main/java/com/example/cjj/ResetPasswordPage.kt
+++ app/src/main/java/com/example/cjj/ResetPasswordPage.kt
@@ -1,7 +1,25 @@
 package com.example.cjj
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.onClick
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.emptyactivity.data.AuthViewModel
 
 @Composable
@@ -10,5 +28,58 @@ fun ResetPasswordPage(
     goToLogin: () -> Unit = {},
     modifier: Modifier = Modifier
 ) {
+    var emailText by rememberSaveable{ mutableStateOf("") }
 
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "Reset Password",
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold,
+        )
+        Text(
+            text = "Type in your email address and we'll send a password reset request to it.",
+            style = MaterialTheme.typography.titleMedium
+        )
+        LoginSignupTextField(
+            label = "Email",
+            placeholder = "example@email.com",
+            onValueChange = { emailText = it },
+        )
+        Button(
+            modifier = Modifier
+                .padding(10.dp)
+                .width(200.dp)
+                .height(50.dp)
+                .semantics {
+                    onClick(label = "Send password reset email.", action = null)
+                },
+            onClick = {
+                authViewModel.sendPasswordResetEmail(emailText)
+            }
+        ) {
+            Text(
+                text = "Send email",
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+        Button(
+            modifier = Modifier
+                .padding(10.dp)
+                .width(200.dp)
+                .height(50.dp)
+                .semantics {
+                    onClick(label = "Go back to login page.", action = null)
+                },
+            onClick = { goToLogin() }
+        ) {
+            Text(
+                text = "Log in",
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+    }
 }
